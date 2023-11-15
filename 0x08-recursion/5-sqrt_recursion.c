888879f9d85582129891670fea3c56246e0f6b30
JudahNuer <91411239+JudahNuer@users.noreply.github.com>
2023-11-15 22:57:26 +0000

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include "main.h"
 /**
  * _sqrt_recursion - Returns the natural square root of a number
  * @n: The number
@@ -8,12 +8,12 @@
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
-		return -1;  // Negative numbers do not have a real square root
+		return (-1);  /* Negative numbers do not have a real square root */
 
 	if (n == 0 || n == 1)
-		return n;  // Base case: square root of 0 or 1 is the number itself
+		return (n);  /* Base case: square root of 0 or 1 is the number itself */
 
-	return _sqrt_helper(n, 2);  // Call the helper function
+	return (_sqrt_helper(n, 2));  /* Call the helper function */
 }
 
 /**
@@ -25,10 +25,10 @@ int _sqrt_recursion(int n)
 int _sqrt_helper(int n, int i)
 {
 	if (i * i == n)
-		return i;  // Found the square root
+		return (i);  /* Found the square root */
 
 	if (i * i > n)
-		return -1;  // The number does not have a natural square root
+		return (-1);  /* The number does not have a natural square root */
 
-	return _sqrt_helper(n, i + 1);  // Try the next candidate
+	return (_sqrt_helper(n, i + 1));  /* Try the next candidate */
 }
